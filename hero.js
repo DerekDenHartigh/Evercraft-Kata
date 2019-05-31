1b4716e46bdcca4792a56ce986f08f6eb824ff83
Derek <derekdenhartigh@gmail.com>
2019-05-31 13:39:38 -0400

    post pizza coding

--- hero.js
+++ hero.js
@@ -1,9 +1,68 @@
 "use strict";
-let hero = {};
+// let hero = {};
 
-let nameHero = ()=>{
-    hero.name = "Bob";
+class Hero {
+    constructor(){
+        // const hero = this;
+        this._name = "Bob";
+        this._alignment = "neutral";
+        this._armor = 10;
+        this._hp = 5;
+        // this._reName = (newName)=>{
+        //     if (newName != "" && newName != null){
+        //         hero.name = newName;
+        //     } else {
+        //         console.log("invalid name entry")
+        //     };
+        // this._reAlign = (newAlignment)=>{
+        //     if (newAlignment==="neutral" || newAlignment==="evil" || newAlignment==="good") {
+        //         this._alignment = newAlignment;
+        //     }
+        //     else {
+        //     console.error(`${newAlignment} is invalid - good, neutral, or evil are acceptable`);
+        //     let error = new Error("invalid value entered");
+        //     throw error;
+        //     };
+        // }
+    }
+
+    get name (){
+        return this._name;
+    }
+
+    set name(newName){
+        this._name = newName;
+    }
+
+    get alignment (){
+        return this._alignment;
+    }
+
+    set alignment(newAlignment){
+        if(newAlignment==="neutral" || newAlignment==="evil" || newAlignment==="good")
+            this._alignment = newAlignment;
+        else {
+            // console.error(`${newAlignment} is invalid - good, neutral, or evil are acceptable`);
+            // let error = new Error("invalid value entered");
+            throw `alignment cannot be ${newAlignment}!`;
+        }
+    }
+
+    get armor(){
+        return this._armor = 10;
+    }
+
+    set armor(newArmorRating){
+        return this._armor = newArmorRating;
+    }
+
+    get hp(){
+        return this._hp = 5;
+    }
+
+    set hp(newHpRating){
+        return this._hp = newHpRating;
+    }
 }
-nameHero();
 
-module.exports= hero;
+module.exports= Hero;
--- spec/hero-spec.js
+++ spec/hero-spec.js
@@ -1,8 +1,53 @@
-describe("name", function() {
-    var hero = require('../hero.js');
+describe("Hero", ()=> {
+    var Hero = require('../hero.js');
   
-    it("hero should have a name", function() {
-        expect(hero.name).toBe("Bob");
+    beforeEach(()=>{
+        this.hero = new Hero();
+    });
+
+    describe("hero's name", ()=>{
+
+        it("hero should have a name", ()=>{
+            expect(this.hero.name).toBe("Bob");
+        });
+    
+        it("SHOULD HAVE a mutatable name", ()=>{
+            this.hero.name = "somethingElse";
+            expect(this.hero.name).toBe("somethingElse");
+        });
+    })
+
+    describe("hero's alignment", ()=>{
+
+        it("alignment should be neutral", ()=>{
+            expect(this.hero.alignment).toBe("neutral");
+        });
+
+        it("allows for alignment to mutate", ()=>{
+            this.hero.alignment = "evil";
+            // this.hero.alignment("evil");
+            expect(this.hero.alignment).toBe("evil");
+        });
+
+        it("rejects unaccaptable alignments", ()=>{
+            this.hero.alignment = "dumb";
+            // expect(this.hero.alignment).toThrowError();
+            expect(()=>{
+                this.alignment("evil");
+            }).toThrowError(`alignment cannot be dumb!`);
+        });
+    });
+
+    describe("hero's armor", ()=>{
+        it("default armor = 10", ()=>{
+            expect(this.hero.armor).toBe(10);
+        })
+    });
+ 
+    describe("hero's hp", ()=>{
+        it("default hp = 5", ()=>{
+            expect(this.hero.hp).toBe(5);
+        })
     });
   
   });
