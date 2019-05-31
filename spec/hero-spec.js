6328f96dbd532c58b3f3cc3f84c82add609e2d60
Derek <derekdenhartigh@gmail.com>
2019-05-31 10:58:28 -0400

    we have our first failure

---
+++ spec/hero-spec.js
@@ -0,0 +1,10 @@
+describe("name", function() {
+    // var hero = require('./hero.js');
+  
+    it("hero should have a name", function() {
+        expect(false).toBe(true)
+        // expect(hero.name).toEqual("Bob");
+    });
+  
+  });
+  
