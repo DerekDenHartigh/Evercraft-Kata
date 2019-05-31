d407777c7b607a6ac509b2a61b3f145f428b77a1
Derek <derekdenhartigh@gmail.com>
2019-05-31 16:57:40 -0400

    As far as Bob and I got today

--- hero.js
+++ hero.js
@@ -2,103 +2,161 @@
 // let hero = {};
 
 class Hero {
-    constructor(){
-        // const hero = this;
-        this._name = "Bob";
-        this._alignment = "neutral";
-        this._armor = 10;
-        this._hp = 5;
-        // this._reName = (newName)=>{
-        //     if (newName != "" && newName != null){
-        //         hero.name = newName;
-        //     } else {
-        //         console.log("invalid name entry")
-        //     };
-        // this._reAlign = (newAlignment)=>{
-        //     if (newAlignment==="neutral" || newAlignment==="evil" || newAlignment==="good") {
-        //         this._alignment = newAlignment;
-        //     }
-        //     else {
-        //     console.error(`${newAlignment} is invalid - good, neutral, or evil are acceptable`);
-        //     let error = new Error("invalid value entered");
-        //     throw error;
-        //     };
-        // }
+  constructor() {
+    // const hero = this;
+    this._name = "Bob";
+    this._alignment = "neutral";
+    this._armor = 10;
+    this._hp = 5;
+    this.exp = 0;
+    this.level = 0; 
+    this.strength = 10;
+    this.dexterity = 10;
+    this.constitution = 10;
+    this.wisdom = 10;
+    this.intelligence = 10;
+    this.charisma = 10;
 
-        this.armor = (newArmorRating)=>{ // gives armor of 10 if string or empty, otherwise assigns new armor rating
-                if(!newArmorRating){
-                    return this._armor = 10;
-                } else {
-                    if(isNaN(newArmorRating) === false){
-                    return this._armor = newArmorRating;
-                    }
-                    else{
-                        return this._armor=10;
-                    }
-                }
-        };
+    this.abilities = ['strength', 'dexterity', 'constitution', 'wisdom', 'intelligence', 'charisma'];
 
-        this.hp = (newHpRating)=>{ // gives hp of 10 if string or empty, otherwise assigns new hp rating
-            if(newHpRating===0){
-                console.log("U R Ded!")
-                return 0;
-            } else if(!newHpRating) {
-                return this._hp = 5;
-            }  
-            else {
-                if(isNaN(newHpRating) === false){
-                return this._hp = newHpRating;
-                }
-                else{
-                    return this._hp=5;
-                }
-            }
+    // this._reName = (newName)=>{
+    //     if (newName != "" && newName != null){
+    //         hero.name = newName;
+    //     } else {
+    //         console.log("invalid name entry")
+    //     };
+    // this._reAlign = (newAlignment)=>{
+    //     if (newAlignment==="neutral" || newAlignment==="evil" || newAlignment==="good") {
+    //         this._alignment = newAlignment;
+    //     }
+    //     else {
+    //     console.error(`${newAlignment} is invalid - good, neutral, or evil are acceptable`);
+    //     let error = new Error("invalid value entered");
+    //     throw error;
+    //     };
+    // }
+
+    this.armor = (newArmorRating) => { // gives armor of 10 if string or empty, otherwise assigns new armor rating
+      if (!newArmorRating) {
+        return this._armor = 10;
+      } else {
+        if (isNaN(newArmorRating) === false) {
+          return this._armor = newArmorRating;
+        }
+        else {
+          return this._armor = 10;
+        }
+      }
     };
 
-        this.attack = (attackDamage)=>{
-            
+    this.hp = (newHpRating) => { // gives hp of 10 if string or empty, otherwise assigns new hp rating
+      if (newHpRating === 0) {
+        console.log("U R Ded!")
+        return 0;
+      } else if (!newHpRating) {
+        return this._hp = 5;
+      }
+      else {
+        if (isNaN(newHpRating) === false) {
+          return this._hp = newHpRating;
         }
-
-
-    }
-
-    get name (){
-        return this._name;
-    }
-
-    set name(newName){
-        return this._name = newName;
-    }
-
-    get alignment (){
-        return this._alignment;
-    }
-
-    set alignment(newAlignment){
-        if(newAlignment==="neutral" || newAlignment==="evil" || newAlignment==="good")
-            this._alignment = newAlignment;
         else {
-            // console.error(`${newAlignment} is invalid - good, neutral, or evil are acceptable`);
-            // let error = new Error("invalid value entered");
-            throw `alignment cannot be ${newAlignment}!`;
+          return this._hp = 5;
         }
+      }
+    };
+
+    this.attack = (attackDamage, opponent) => {
+      if (attackDamage === 20) {
+        opponent.hp -= 2;
+        this.gainExp();
+        return opponent.hp;
+      } else if (attackDamage >= opponent.armor) {
+        this.gainExp();
+        opponent.hp -= 1;
+      } else {
+        return opponent.hp;
+      }
     }
 
-    // get armor(){
-    //     return this._armor = 10;
-    // }
+    this.gainExp = () => {
+      this.exp += 10;
+      if (!(this.exp % 1000)) {
+        this.level += 1;
+        this.checkLevel();
+      }
 
-    // set armor(newArmorRating){
-    //     return this._armor = newArmorRating;
-    // }
+    }
 
-    // get hp(){
-    //     return this._hp = 5;
-    // }
+    this.checkLevel = () => {
+      if ( (this.level % 2) === 0) {
+        this.attack_modifer += 1;
+      }
+    }
 
-    // set hp(newHpRating){
-    //     return this._hp = newHpRating;
-    // }
+
+
+    this.isDead = (opponent) => {
+      if (opponent.hp <= 0) {
+        return 'you are dead';
+      } else {
+        return opponent.hp;
+      }
+    }
+
+    this.abilityModifier = (score, ability, name) => {
+      let modifiers = [null, -5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2,  3, 3, 4, 4, 5];
+      // use index position to correspond to die roll
+            
+      if ( name.abilities.includes(ability)) {
+        name[ability] += modifiers[score];    
+      } else {
+        return 'banana';
+      }
+
+    }
+
+
+
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    return this._name = newName;
+  }
+
+  get alignment() {
+    return this._alignment;
+  }
+
+  set alignment(newAlignment) {
+    if (newAlignment === "neutral" || newAlignment === "evil" || newAlignment === "good")
+      this._alignment = newAlignment;
+    else {
+      // console.error(`${newAlignment} is invalid - good, neutral, or evil are acceptable`);
+      // let error = new Error("invalid value entered");
+      throw `alignment cannot be ${newAlignment}!`;
+    }
+  }
+
+  // get armor(){
+  //     return this._armor = 10;
+  // }
+
+  // set armor(newArmorRating){
+  //     return this._armor = newArmorRating;
+  // }
+
+  // get hp(){
+  //     return this._hp = 5;
+  // }
+
+  // set hp(newHpRating){
+  //     return this._hp = newHpRating;
+  // }
 }
 
-module.exports= Hero;
+module.exports = Hero;
--- spec/hero-spec.js
+++ spec/hero-spec.js
@@ -1,81 +1,133 @@
-describe("Hero", ()=> {
+describe("Hero", () => {
     var Hero = require('../hero.js');
-  
-    beforeEach(()=>{
-        this.hero = new Hero();
-        this.opponent = new Hero();
+    beforeEach(() => {
+      this.hero = new Hero();
+      this.opponent = new Hero();
     });
-
-    describe("hero's name", ()=>{
-
-        it("hero should have a name", ()=>{
-            expect(this.hero.name).toBe("Bob");
-        });
+    describe("hero's name", () => {
+      it("hero should have a name", () => {
+        expect(this.hero.name).toBe("Bob");
+      });
+      it("SHOULD HAVE a mutatable name", () => {
+        this.hero.name = "somethingElse";
+        expect(this.hero.name).toBe("somethingElse");
+      });
+    })
+    describe("hero's alignment", () => {
+      it("alignment should be neutral", () => {
+        expect(this.hero.alignment).toBe("neutral");
+      });
+      it("allows for alignment to mutate", () => {
+        this.hero.alignment = "evil";
+        // this.hero.alignment("evil");
+        expect(this.hero.alignment).toBe("evil");
+      });
+      it("rejects unaccaptable alignments", () => {
+        this.hero.alignment = "dumb";
+        // expect(this.hero.alignment).toThrowError();
+        expect(() => {
+          this.alignment("evil");
+        }).toThrowError(`alignment cannot be dumb!`);
+      });
+    });
+    describe("hero's armor", () => {
+      it("default armor = 10", () => {
+        expect(this.hero.armor()).toEqual(10);
+      });
+      it("armor updates to 30", () => {
+        expect(this.hero.armor(30)).toEqual(30);
+      });
+      it("armor shouldn't update to zebra", () => {
+        expect(this.hero.armor("zebra")).toBe(10);
+      });
+    });
+    describe("hero's hp", () => {
+      it("default hp = 5", () => {
+        expect(this.hero.hp()).toEqual(5);
+      });
+      it("hp updates to 30", () => {
+        expect(this.hero.hp(30)).toEqual(30);
+      });
+      it("hp shouldn't update to zebra", () => {
+        expect(this.hero.hp("zebra")).toBe(5);
+      });
+      it("hp should return 0 w/ U R ded message if your hp = 0", () => {
+        expect(this.hero.hp(0)).toBe(0);
+      })
+    });
+    describe("combat time", () => {
+      it("hero attcking unarmored opponent, reduces their hp if attack meets/beats opponent armor class", () => {
+        this.opponent.hp = 10;
+        this.opponent.armor = 0;
+        this.hero.attack(5, this.opponent);
+        expect(this.opponent.hp).toBe(9);
+      })
+      it("hero attacking armored opponent, reduces their hp if attack meets/beats opponent armor class", () => {
+        this.opponent.hp = 10;
+        this.opponent.armor = 5;
+        this.hero.attack(5, this.opponent);
+        expect(this.opponent.hp).toBe(9);
+      })
+      it("hero attacking heavily armored opponent, nothing happens cuz attack deflected", () => {
+        this.opponent.hp = 10;
+        this.opponent.armor = 20;
+        this.hero.attack(5, this.opponent);
+        expect(this.opponent.hp).toBe(10);
+      })
+      it("if hero attacking at 20, deals double damage", () => {
+        this.opponent.hp = 10;
+        this.opponent.armor = 20;
+        this.hero.attack(20, this.opponent);
+        expect(this.opponent.hp).toBe(8);
+      })
+      it("if hero has 0 hp, they are dead", () => {
+        this.opponent.hp = 0;
+        this.hero.isDead(this.opponent);
+        expect(this.opponent.isDead(this.opponent)).toBe('you are dead');
+      })
+      it("if hero has 9 hp, they are not dead", () => {
+        this.opponent.hp = 9;
+        this.hero.isDead(this.opponent);
+        expect(this.opponent.isDead(this.opponent)).toBe(9);
+      })
+    })
+    describe("character ability scores", () => {
+      it("roll die, and modify character ability value accordingly", () => {
+        //this.hero.charisma = 10;
+        this.hero.abilityModifier(5, 'charisma', this.hero);
+        expect(this.hero.charisma).toBe(7);
+      })
+      it("check for non-existent ability", () => {
+        expect(this.hero.abilityModifier(5, 'acrobatic', this.hero)).toBe('banana');
+      })
     
-        it("SHOULD HAVE a mutatable name", ()=>{
-            this.hero.name = "somethingElse";
-            expect(this.hero.name).toBe("somethingElse");
-        });
     })
-
-    describe("hero's alignment", ()=>{
-
-        it("alignment should be neutral", ()=>{
-            expect(this.hero.alignment).toBe("neutral");
-        });
-
-        it("allows for alignment to mutate", ()=>{
-            this.hero.alignment = "evil";
-            // this.hero.alignment("evil");
-            expect(this.hero.alignment).toBe("evil");
-        });
-
-        it("rejects unaccaptable alignments", ()=>{
-            this.hero.alignment = "dumb";
-            // expect(this.hero.alignment).toThrowError();
-            expect(()=>{
-                this.alignment("evil");
-            }).toThrowError(`alignment cannot be dumb!`);
-        });
-    });
-
-    describe("hero's armor", ()=>{
-        it("default armor = 10", ()=>{
-            expect(this.hero.armor()).toEqual(10);
-        });
-
-        it("armor updates to 30", ()=>{
-            expect(this.hero.armor(30)).toEqual(30);
-        });
-        it("armor shouldn't update to zebra", ()=>{
-            expect(this.hero.armor("zebra")).toBe(10);
-        });
-    });
- 
-    describe("hero's hp", ()=>{
-        it("default hp = 5", ()=>{
-            expect(this.hero.hp()).toEqual(5);
-        });
-
-        it("hp updates to 30", ()=>{
-            expect(this.hero.hp(30)).toEqual(30);
-        });
-        it("hp shouldn't update to zebra", ()=>{
-            expect(this.hero.hp("zebra")).toBe(5);
-        });
-        it("hp should return 0 w/ U R ded message if your hp = 0", ()=>{
-            expect(this.hero.hp(0)).toBe(0);
-        })
-    });
-
-    describe("combat time", ()=>{
-        it("hero attcking unarmored opponent, reduces their hp if attack meets/beats opponent armor class", ()=>{
-            this.hero.attack(5);
-            this.opponent.hp = 10;
-            this.opponent.armor = 5;
-            expect(this.opponent.hp).toBe(5);
-        })
+    // Feature: Character Ability Modifiers Modify Attributes -- TO BE CONTINUED
+    describe("character ability scores", () => {
+      it("roll die, and modify character ability value accordingly", () => {
+        //this.hero.charisma = 10;
+        this.hero.abilityModifier(5, 'charisma', this.hero);
+        expect(this.hero.charisma).toBe(7);
+      })  
     })
-  
+    describe("character experience", () => {
+      it("A Character can gain experience when attacking", () => {
+        this.hero.attack(20, this.opponent);
+        expect(this.hero.exp).toBe(10);
+      })
+    
+    });
+    describe("character levels", () => {
+      it("A character can level up to level 2", () => {
+        this.hero.exp = 990;
+        this.hero.attack(20, this.opponent);
+        expect(this.hero.level).toBe(2);
+      })
+    
+      it("When leveling up, 1 is added to attack roll if level is even", () => {
+        this.hero.exp = 1990;
+        this.hero.attack(20, this.opponent);
+        expect(this.hero.attack_modifier).toBe(1);
+      })
+    });
   });
-  
